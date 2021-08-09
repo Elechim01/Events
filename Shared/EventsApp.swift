3f7223471922c4faaba5beae89750da7c9447a1a
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-09 22:22:36 +0200

    Initial Commit

---
+++ Shared/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
